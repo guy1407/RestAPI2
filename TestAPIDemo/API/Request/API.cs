bb7136f139344a73b434bea1574fa26261804957
Guy Refael Cohen <guy1407@gmail.com>
2021-09-27 09:56:12 +0300

    Add project files.

---
+++ TestAPIDemo/API/Request/API.cs
@@ -0,0 +1,21 @@
+﻿namespace TestAPIDemo.API.Request
+{
+   class API
+   {
+
+      private System.Net.Http.HttpClient  restClient = new System.Net.Http.HttpClient();
+
+      private string URI = "https://api.publicapis.org";
+
+      public async System.Threading.Tasks.Task<string> testRequest()
+      {
+         var Builder = new System.UriBuilder($"{URI}/entries?category=animals&https=true");
+
+         var response = await restClient.GetAsync(Builder.Uri);
+
+         var context = await response.Content.ReadAsStringAsync();
+
+         return context;
+      }
+   }
+}
---
+++ TestAPIDemo/APITests/Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAPIDemo.APITests
+{
+   public class Tests
+   {
+      [NUnit.Framework.Category("Test")]
+      [NUnit.Framework.Test]
+      public async System.Threading.Tasks.Task TestAPICall()
+      {
+         TestAPIDemo.API.Request.API api = new TestAPIDemo.API.Request.API();
+
+         var response = await api.testRequest();
+      }
+   }
+}
